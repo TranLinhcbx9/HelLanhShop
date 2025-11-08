f20057f955902653553156d0f2dfcccd7731d9e2
TranLinhcbx9 <linhtran.cbx9@gmail.com>
2025-11-08 21:19:05 +0700

    add Unit of work

---
+++ HelLanhShop.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using HelLanhShop.Application.ComboTemplateItems.Interfaces;
+using HelLanhShop.Application.ComboTemplates.Interfaces;
+using HelLanhShop.Application.Customers.Interfaces;
+using HelLanhShop.Application.Employees.Interfaces;
+using HelLanhShop.Application.InventoryEntries.Interfaces;
+using HelLanhShop.Application.InventoryEntryDetails.Interfaces;
+using HelLanhShop.Application.Products.Interfaces;
+using HelLanhShop.Application.SaleDetails.Interfaces;
+using HelLanhShop.Application.Sales.Interfaces;
+using HelLanhShop.Application.Suppliers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelLanhShop.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IComboTemplateItemRepository ComboTemplateItems { get; }
+        IComboTemplateRepository ComboTemplates { get; }
+        ICustomerRepository Customers { get; }
+        IEmployeeRepository Employees { get; }
+        IInventoryEntryDetailRepository InventoryEntryDetails { get; }
+        IInventoryEntryRepository InventoryEntries { get; }
+        IProductRepository Products { get; }
+        ISaleDetailRepository SaleDetails { get; }
+        ISaleRepository Sales { get; }
+        ISupplierRepository Suppliers { get; }
+        Task<int> SaveChangesAsync();
+
+    }
+}
